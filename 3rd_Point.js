fc84fc64d33b8ed1b1829c74e6b9ce5ba06435bb
oysierra <oysierra@gmail.com>
2023-05-06 06:04:10 -0500

    3rd fixed

--- 3rd_Point.js
+++ 3rd_Point.js
@@ -1,9 +1,63 @@
-function Typeof_Elements(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (typeof arr[i] === "number") {
-            sum += arr[i];
-        }
+function PilaStack() {
+    // 1. Constructor
+    // se crea el constructor con los atributos basicos que necesita la clase
+    //constructor(){
+    this.elementos = [];
+    //}
+    // 2. Insertar
+    //insertar(elemento){
+    this.insertar = function (elemento) {
+        // se agrega el elemento al arreglo
+        this.elementos.push(elemento);
     }
-    return sum;
+    // 3. Extraer
+    //extraer(){
+    this.extraer = function () {
+        // se toma el ultimo elemento con el tamaño - 1 recordar que los arreglos empiezan en 0 no en 1
+        const ultimo = this.elementos[this.elementos.length - 1];
+        // se elimina del arreglo el ultimo elemento ingresado
+        this.elementos.pop();
+        // devuelve el ultimo elemento
+        return ultimo;
+    };
+    // 4. Longitud
+    //longitud(){
+    this.longitud = function () {
+        // tamaño de la pila
+        return this.elementos.length;
+    }
+    // 5. Ver proximo
+    this.ver_proximo = function () {
+        // se mira el siguiente elemento a salir de la pila pero sin eliminarlo del arreglo
+        return this.elementos[this.elementos.length - 1];
+    }
+    // 6. Ver Elementos o toda la pila
+    this.ver_elementos = function () {
+        // se mira toda la pila como esta
+        return this.elementos;
+    }
+
+
+    this.sumar_numeros = function () {
+        let suma = 0;
+        for (let i = 0; i < this.elementos.length; i++) {
+            const numero = this.elementos[i];
+            if (!isNaN(numero)) {
+                suma += numero;
+            }
+        }
+        return suma;
+    };
 }
+
+const pila = new PilaStack();
+
+var longitud = prompt("Cuantos elementos desea ingresar")
+
+for (let i = 0; i < longitud; i++) {
+
+    var n = parseInt(prompt("Ingrese un elemento"));
+    pila.insertar(n);
+}
+console.log(pila.ver_elementos());
+pila.sumar_numeros();
