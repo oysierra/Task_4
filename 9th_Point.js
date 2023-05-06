4c52977110fd44090c9d94e239aff9a920fa17a2
oysierra <oysierra@gmail.com>
2023-05-06 06:35:47 -0500

    9th fixed

--- 9th_Point.js
+++ 9th_Point.js
@@ -1,10 +1,64 @@
-function removeElementByIndex(arr, index) {
-    if (index >= 0 && index < arr.length) {
-        arr.splice(index, 1);
-        return "Elemento en la posición " + index + " eliminado correctamente";
-    } else {
-        return "La posición " + index + " no existe en la fila";
+// Pila-Stack
+// Lista ordenada o estructura de datos que permite almacenar y recuperar datos
+
+//1.[] Constructor
+//2.[] Insertar
+//3.[] Extraer
+//4.[] Longitud
+//5.[] Ver proximo
+//6.[] Ver Elementos o toda la pila
+
+function PilaStack() {
+    // 1. Constructor
+    this.elementos = [];
+
+    // 2. Insertar
+    this.insertar = function (elemento) {
+        this.elementos.push(elemento);
+    }
+
+    // 3. Extraer
+    this.extraer = function () {
+        const ultimo = this.elementos[this.elementos.length - 1];
+        this.elementos.pop();
+        return ultimo;
+    };
+
+    // 4. Longitud
+    this.longitud = function () {
+        return this.elementos.length;
+    }
+
+    // 5. Ver proximo
+    this.ver_proximo = function () {
+        return this.elementos[this.elementos.length - 1];
+    }
+
+    // 6. Ver Elementos o toda la pila
+    this.ver_elementos = function () {
+        return this.elementos;
+    }
+
+    this.eliminar_por_posicion = function (posicion) {
+        if (posicion < 1 || posicion > this.elementos.length) {
+            console.log("Posición inválida. No se puede eliminar elemento.");
+            return;
+        }
+
+        this.elementos.splice(posicion - 1, 1);
     }
 }
 
-console.log(removeElementByIndex(["world", "warcraft", "ACDC"], 2));
+const pila = new PilaStack();
+
+
+console.log(pila.ver_elementos());
+
+
+pila.insertar(1);
+pila.insertar('dos');
+pila.insertar(3);
+pila.insertar('cuatro');
+pila.eliminar_por_posicion(5)
+
+console.log(pila.ver_elementos());
