b2fe758419678542d104eee031a5026aa9f49c54
oysierra <oysierra@gmail.com>
2023-05-06 06:17:54 -0500

    6th fixed

--- 6th_Point.js
+++ 6th_Point.js
@@ -1,11 +1,73 @@
-function getStringElements(arr) {
-    let stringElements = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (typeof arr[i] === "string") {
-            stringElements.push(arr[i]);
-        }
+// Pila-Stack
+// Lista ordenada o estructura de datos que permite almacenar y recuperar datos
+
+//1.[] Constructor
+//2.[] Insertar
+//3.[] Extraer
+//4.[] Longitud
+//5.[] Ver proximo
+//6.[] Ver Elementos o toda la pila
+
+function PilaStack() {
+    // 1. Constructor
+    // se crea el constructor con los atributos basicos que necesita la clase
+    //constructor(){
+    this.elementos = [];
+    //}
+    // 2. Insertar
+    //insertar(elemento){
+    this.insertar = function (elemento) {
+        // se agrega el elemento al arreglo
+        this.elementos.push(elemento);
     }
-    return stringElements;
+    // 3. Extraer
+    //extraer(){
+    this.extraer = function () {
+        // se toma el ultimo elemento con el tamaño - 1 recordar que los arreglos empiezan en 0 no en 1
+        const ultimo = this.elementos[this.elementos.length - 1];
+        // se elimina del arreglo el ultimo elemento ingresado
+        this.elementos.pop();
+        // devuelve el ultimo elemento
+        return ultimo;
+    };
+    // 4. Longitud
+    //longitud(){
+    this.longitud = function () {
+        // tamaño de la pila
+        return this.elementos.length;
+    }
+    // 5. Ver proximo
+    this.ver_proximo = function () {
+        // se mira el siguiente elemento a salir de la pila pero sin eliminarlo del arreglo
+        return this.elementos[this.elementos.length - 1];
+    }
+    // 6. Ver Elementos o toda la pila
+    this.ver_elementos = function () {
+        // se mira toda la pila como esta
+        return this.elementos;
+    }
+
+
+    this.obtener_string = function () {
+        const cadenas = [];
+        for (let i = 0; i < this.elementos.length; i++) {
+            if (typeof this.elementos[i] === 'string') {
+                cadenas.push(this.elementos[i]);
+            }
+        }
+        return cadenas;
+    };
 }
 
-console.log(Get_StringElements([3, "pepe", true, "mundo", 7, "10", 77]));
+const pila = new PilaStack();
+
+
+console.log(pila.ver_elementos());
+pila.obtener_string();
+
+pila.insertar(1);
+pila.insertar('dos');
+pila.insertar(3);
+pila.insertar('cuatro');
+
+console.log(pila.obtener_string());
