85f66213af190c12a26abecfb2f4b2f5128a4a7a
oysierra <oysierra@gmail.com>
2023-05-05 19:42:36 -0500

    Modificacion ejercicio 1

--- 1st_Point.js
+++ 1st_Point.js
@@ -1,9 +1,40 @@
-function Arrleglo_Numeros_Consecutivos(n) {
-    let arr = [];
-    for (let i = 1; i <= n; i++) {
-        arr.push(i);
+// Pila-Stack
+// Lista ordenada o estructura de datos que permite almacenar y recuperar datos
+
+//1.[] Constructor
+//2.[] Insertar
+//3.[] Extraer
+//4.[] Longitud
+//5.[] Ver proximo
+//6.[] Ver Elementos o toda la pila
+
+function PilaStack() {
+    // 1. Constructor
+    // se crea el constructor con los atributos basicos que necesita la clase
+    //constructor(){
+    this.elementos = [];
+    //}
+    // 2. Insertar
+    //insertar(elemento){
+    this.insertar = function (elemento) {
+        // se agrega el elemento al arreglo
+        this.elementos.push(elemento);
     }
-    return arr;
+
+    // 6. Ver Elementos o toda la pila
+    this.ver_elementos = function () {
+        // se mira toda la pila como esta
+        return this.elementos;
+    }
 }
 
-console.log(Arrleglo_Numeros_Consecutivos(4));
+const pila = new PilaStack();
+var longitud = prompt("Cuantos elementos desea ingresar")
+
+for (let i = 0; i < longitud; i++) {
+
+    var n = prompt("Ingrese un elemento");
+    pila.insertar({ url: n });
+}
+
+console.log(pila.ver_elementos());
