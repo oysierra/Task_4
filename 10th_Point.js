16075320db95d3b6a298d8f50b06d8c574c2f394
oysierra <oysierra@gmail.com>
2023-05-06 06:40:37 -0500

    10th fixed

--- 10th_Point.js
+++ 10th_Point.js
@@ -1,10 +1,71 @@
-function removeElementsUntilIndex(arr, index) {
-    if (index >= 0 && index < arr.length) {
-        arr.splice(0, index + 1);
-        return "Elementos hasta la posición " + index + " eliminados correctamente";
-    } else {
-        return "La posición " + index + " no existe en la fila";
+// Pila-Stack
+// Lista ordenada o estructura de datos que permite almacenar y recuperar datos
+
+//1.[] Constructor
+//2.[] Insertar
+//3.[] Extraer
+//4.[] Longitud
+//5.[] Ver proximo
+//6.[] Ver Elementos o toda la pila
+
+function PilaStack() {
+    // 1. Constructor
+    this.elementos = [];
+
+    // 2. Insertar
+    this.insertar = function (elemento) {
+        this.elementos.push(elemento);
+    }
+
+    // 3. Extraer
+    this.extraer = function () {
+        const ultimo = this.elementos[this.elementos.length - 1];
+        this.elementos.pop();
+        return ultimo;
+    };
+
+    // 4. Longitud
+    this.longitud = function () {
+        return this.elementos.length;
+    }
+
+    // 5. Ver proximo
+    this.ver_proximo = function () {
+        return this.elementos[this.elementos.length - 1];
+    }
+
+    // 6. Ver Elementos o toda la pila
+    this.ver_elementos = function () {
+        return this.elementos;
+    }
+
+    this.eliminar_hasta_posicion = function(posicion) {
+        if (posicion < 0 || posicion >= this.elementos.length) {
+            console.log("La posición dada no existe en la fila.");
+            return;
+        }
+        
+        // Eliminar los elementos hasta la posición dada
+        this.elementos.splice(0, posicion+1);
+    
+        // Reindexar los elementos restantes
+        for (let i = 0; i < this.elementos.length; i++) {
+            this.elementos[i] = this.elementos[i + posicion];
+        }
+        this.elementos.length = this.elementos.length - posicion;
     }
 }
 
-console.log(removeElementsUntilIndex(["apple", "samsung", "msi", "asus", "xiaomi"], 2));
+const pila = new PilaStack();
+
+pila.insertar(1);
+pila.insertar('dos');
+pila.insertar(3);
+pila.insertar('cuatro');
+pila.insertar('cinto');
+pila.insertar(7);
+pila.insertar('warcraft');
+console.log(pila.ver_elementos());
+pila.eliminar_hasta_posicion(3);
+
+console.log(pila.ver_elementos());
